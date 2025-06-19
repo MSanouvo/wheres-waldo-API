65461df1e91ba59a9849060b75a1964c75765a6d
MSanouvo <43829596+MSanouvo@users.noreply.github.com>
2025-06-18 18:27:45 -0700

    feat: Moved to Neon Database for production
    - updated schema with new  database url
    - reran database scripts to add game information
    to new database

--- database/dbScript.js
+++ database/dbScript.js
@@ -21,94 +21,95 @@ async function addTestMap() {
             url: "/static/assets/maps/map1/wimmelbilder.png"
         }
     })
+    console.log(map)
 }
 
 async function addTestTarget() {
-    const target1 = await prisma.targets.update({
-        where: {
-            name: 'Target 1',
-            id: 6
-        },
+    // const target1 = await prisma.targets.update({
+    //     where: {
+    //         name: 'Target 1',
+    //         id: 6
+    //     },
+    //     data: {
+    //         x_min: 850,
+    //         x_max: 900,
+    //         y_min: 200,
+    //         y_max: 250
+    //     }
+    // })
+    // const target2 = await prisma.targets.update({
+    //     where: {
+    //         name: 'Target 2',
+    //         id: 7
+    //     },
+    //     data: {
+    //         x_min: 560,
+    //         x_max: 620,
+    //         y_min: 400,
+    //         y_max: 460
+    //     }
+    // })
+    // const target3 = await prisma.targets.update({
+    //     where: {
+    //         name: 'Target 3',
+    //         id: 8
+    //     },
+    //     data: {
+    //         x_min: 375,
+    //         x_max: 415,
+    //         y_min: 470,
+    //         y_max: 520
+    //     }
+    // })
+    const target1 = await prisma.targets.create({
         data: {
+            name: "Target 1",
+            mapId: 1,
             x_min: 850,
             x_max: 900,
             y_min: 200,
             y_max: 250
         }
     })
-    const target2 = await prisma.targets.update({
-        where: {
-            name: 'Target 2',
-            id: 7
-        },
+    const target2 = await prisma.targets.create({
         data: {
+            name: "Target 2",
+            mapId: 1,
             x_min: 560,
             x_max: 620,
             y_min: 400,
             y_max: 460
         }
     })
-    const target3 = await prisma.targets.update({
-        where: {
-            name: 'Target 3',
-            id: 8
-        },
+    const target3 = await prisma.targets.create({
         data: {
+            name: "Target 3",
+            mapId: 1,
             x_min: 375,
             x_max: 415,
             y_min: 470,
             y_max: 520
         }
     })
-    // const target1 = await prisma.targets.create({
-    //     data: {
-    //         name: "Target 1",
-    //         mapId: 4,
-    //         x_min: 100,
-    //         x_max: 200,
-    //         y_min: 200,
-    //         y_max: 300
-    //     }
-    // })
-    // const target2 = await prisma.targets.create({
-    //     data: {
-    //         name: "Target 2",
-    //         mapId: 4,
-    //         x_min: 400,
-    //         x_max: 500,
-    //         y_min: 400,
-    //         y_max: 500
-    //     }
-    // })
-    // const target3 = await prisma.targets.create({
-    //     data: {
-    //         name: "Target 3",
-    //         mapId: 4,
-    //         x_min: 600,
-    //         x_max: 700,
-    //         y_min: 600,
-    //         y_max: 700
-    //     }
-    // })
     
 }
 
 async function addTestIcons(){
     const icon1 = await prisma.icons.create({
         data:{
-            targetId: 6,
+            targetId: 1,
             url: "/static/assets/maps/map1/targets/Target1.png"
         }
     })
     const icon2 = await prisma.icons.create({
         data:{
-            targetId: 7,
+            targetId: 2,
             url: "/static/assets/maps/map1/targets/Target2.png"
         }
     })
     const icon3 = await prisma.icons.create({
         data:{
-            targetId: 8,
+            targetId: 3,
             url: "/static/assets/maps/map1/targets/Target3.png"
         }
     })
